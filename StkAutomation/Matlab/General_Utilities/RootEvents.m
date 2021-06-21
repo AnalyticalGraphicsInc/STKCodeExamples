a3f1e976eb5e9b3e702917cfa8b7337c877270c6
Noah Ingwersen <ningwersen@agi.com>
2021-06-21 18:38:55 -0400

    Added RootEvents.m

---
+++ StkAutomation/Matlab/General_Utilities/RootEvents.m
@@ -0,0 +1,47 @@
+function RootEvents(varargin)
+
+% Full documentation of AgStkObjectRoot events:
+% https://help.agi.com/stkdevkit/index.htm#DocX/STKObjects~AgStkObjectRoot.html?
+
+place = size(varargin);
+spot = place(2);
+
+switch (varargin{spot})
+    
+    case 'OnAnimUpdate'
+        disp('Animation is updating')
+        
+    case 'OnScenarioNew'
+        disp('New Scenario Created')
+        
+    case 'OnScenarioLoad'
+        disp('Scenario Loaded')
+        
+    case 'OnScenarioClose'
+        disp('Scenario Closed')
+        
+    case 'OnScenarioSave'
+        disp('Scenario Saved')
+        
+    case 'OnLogMessage'
+        disp('New Message in Log')
+        
+    case 'OnStkObjectAdded'
+        disp('New Object Added')
+        
+    case 'OnStkObjectDeleted'
+        disp('Object Deleted')
+        
+    case 'OnStkObjectRenamed'
+        disp('Object Name Changed')
+        
+    case 'OnAnimationRewind'
+        disp('Animation Reset')
+        
+    case 'OnAnimationPause'
+        disp('Animation is Paused')
+        
+    case 'OnAnimationPlayback'
+        disp('Animation Playback Event')
+end
+end
