32065b155b38aec0498ad62da64fd6ec46ed5203
Ingwersen <ningwersen@agi.com>
2021-08-24 12:05:09 -0400

    Added PullDataFromSTKExample

---
+++ StkAutomation/CSharp/General_Utilities/PullDataFromSTKExample/PullDataFromSTKExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PullDataFromSTKExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Analytical Graphic Inc.")]
+[assembly: AssemblyProduct("PullDataFromSTKExample")]
+[assembly: AssemblyCopyright("Copyright © Analytical Graphic Inc. 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d9f55be-ec7d-4d54-91a3-e7eb3682cdbe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
