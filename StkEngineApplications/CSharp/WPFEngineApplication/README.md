40f4eb842e623b756aa243570d893843aee7b3c2
Tyler Greenwood <tygree@umich.edu>
2021-12-29 09:52:40 -0500

    Added WPF example application for FAQ

---
+++ StkEngineApplications/CSharp/WPFEngineApplication/README.md
@@ -0,0 +1,7 @@
+Steps to get example running:
+
+Navigate in the sourcecode and find GlobeControl.rex and MapControl.resx. Open the properties of each and make sure to unblock both files
+
+Inside the solution file navigate to references and make sure all AGI Interop dll's all have Embed Interop Types set to False
+
+Rebuild the solution.
---
+++ StkEngineApplications/CSharp/WPFEngineApplication/WPFEngineApplication/MapControl.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WPFEngineApplication
+{
+    partial class MapControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapControl));
+            this.axAgUiAx2DCntrl1 = new AGI.STKX.Controls.AxAgUiAx2DCntrl();
+            this.SuspendLayout();
+            // 
+            // axAgUiAx2DCntrl1
+            // 
+            this.axAgUiAx2DCntrl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.axAgUiAx2DCntrl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axAgUiAx2DCntrl1.Location = new System.Drawing.Point(0, 0);
+            this.axAgUiAx2DCntrl1.Name = "axAgUiAx2DCntrl1";
+            this.axAgUiAx2DCntrl1.Picture = ((System.Drawing.Image)(resources.GetObject("axAgUiAx2DCntrl1.Picture")));
+            this.axAgUiAx2DCntrl1.Size = new System.Drawing.Size(400, 300);
+            this.axAgUiAx2DCntrl1.TabIndex = 0;
+            // 
+            // MapControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.axAgUiAx2DCntrl1);
+            this.Name = "MapControl";
+            this.Size = new System.Drawing.Size(400, 300);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private AGI.STKX.Controls.AxAgUiAx2DCntrl axAgUiAx2DCntrl1;
+    }
+}
