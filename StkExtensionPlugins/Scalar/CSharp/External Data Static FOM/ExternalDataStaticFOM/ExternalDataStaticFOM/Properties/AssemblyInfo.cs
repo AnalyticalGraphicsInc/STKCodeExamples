ddfb89f2e3f92651b46c54c91635f747e0e93ded
Lemme <alemme@agi.com>
2022-01-03 07:54:30 -0500

    Updated External Data Figure of Merit Plugin
    Updated to working version and added read me to clarify how to use it.

--- StkExtensionPlugins/Scalar/CSharp/External Data Static FOM/ExternalDataStaticFOM/ExternalDataStaticFOM/Properties/AssemblyInfo.cs
+++ StkExtensionPlugins/Scalar/CSharp/External Data Static FOM/ExternalDataStaticFOM/ExternalDataStaticFOM/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4acc3414-18e6-492b-92f5-a3c18933aadc")]
