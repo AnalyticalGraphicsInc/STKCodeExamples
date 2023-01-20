4fc0bc107d9c83e82a467ac1b0c538f86bb88ed9
jpthompson212 <jpthompson212@gmail.com>
2023-01-20 17:26:32 -0500

    Ran black on all python ui plugin scripts

--- StkUiPlugins/CSharp/OperatorsToolBox/Stk12.OperatorsToolBox/Plugin Files/ResetAnimationTime.py
+++ StkUiPlugins/CSharp/OperatorsToolBox/Stk12.OperatorsToolBox/Plugin Files/ResetAnimationTime.py
@@ -2,6 +2,6 @@ from comtypes.client import GetActiveObject
 import sys
 
 print(sys.argv)
-uiApplication = GetActiveObject('STK12.Application')
+uiApplication = GetActiveObject("STK12.Application")
 root = uiApplication.Personality2
 root.ExecuteCommand("Animate * Reset")
