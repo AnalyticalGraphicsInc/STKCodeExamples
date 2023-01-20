91ced1c7c719fe82cecf4649cc5b3d8e4db29247
jpthompson212 <jpthompson212@gmail.com>
2023-01-20 17:23:38 -0500

    Ran black on all python extension plugin samples

--- StkExtensionPlugins/AttitudeSimulatorPlugin/FeedbackControl_Targeting/Scripting/Attitude/PY_feedbackTargeting.py
+++ StkExtensionPlugins/AttitudeSimulatorPlugin/FeedbackControl_Targeting/Scripting/Attitude/PY_feedbackTargeting.py
@@ -5,56 +5,62 @@ global PY_feedbackTargeting_Inputs
 
 PY_feedbackTargeting_init = -1
 
-#==========================================================================
+# ==========================================================================
 # PY_feedbackTargeting() fctn
-#==========================================================================
-def PY_feedbackTargeting ( argList ):
-	callMode = str(argList[0])
-	if callMode == 'None' :
-		retVal = PY_feedbackTargeting_compute( argList )	# do compute
-	elif callMode == 'register' :
-		global PY_feedbackTargeting_init 
-		PY_feedbackTargeting_init = -1
-		retVal = PY_feedbackTargeting_register()
-	elif callMode == 'compute' :
-		retVal = PY_feedbackTargeting_compute( argList )	# do compute
-	else:
-		retVal = []	# # bad call, return empty list
-	return retVal
+# ==========================================================================
+def PY_feedbackTargeting(argList):
+    callMode = str(argList[0])
+    if callMode == "None":
+        retVal = PY_feedbackTargeting_compute(argList)  # do compute
+    elif callMode == "register":
+        global PY_feedbackTargeting_init
+        PY_feedbackTargeting_init = -1
+        retVal = PY_feedbackTargeting_register()
+    elif callMode == "compute":
+        retVal = PY_feedbackTargeting_compute(argList)  # do compute
+    else:
+        retVal = []  # # bad call, return empty list
+    return retVal
 
 
 def PY_feedbackTargeting_register():
-	arg1 = 'ArgumentType = Output ; Type = Parameter ; ArgumentName = Torque ; Name = Torque ; BasicType = Vector '
-	arg2 = 'ArgumentType = Input ; ArgumentName = time ; Name = Epoch '
-	arg3 = 'ArgumentType = Input ; ArgumentName = att ; Type = Attitude ; Derivative = Yes '
-	arg4 = 'ArgumentType = Input ; ArgumentName = erratt ; Type = Attitude ; RefName = Body ; RefSource = Satellite/PerfectPointing '
-	arg5 = 'ArgumentType = Input ; ArgumentName = IMtx ; Type = Inertia ; Name = Inertia '
-	argList = [arg1, arg2, arg3, arg4, arg5]
-	return argList
+    arg1 = "ArgumentType = Output ; Type = Parameter ; ArgumentName = Torque ; Name = Torque ; BasicType = Vector "
+    arg2 = "ArgumentType = Input ; ArgumentName = time ; Name = Epoch "
+    arg3 = "ArgumentType = Input ; ArgumentName = att ; Type = Attitude ; Derivative = Yes "
+    arg4 = "ArgumentType = Input ; ArgumentName = erratt ; Type = Attitude ; RefName = Body ; RefSource = Satellite/PerfectPointing "
+    arg5 = (
+        "ArgumentType = Input ; ArgumentName = IMtx ; Type = Inertia ; Name = Inertia "
+    )
+    argList = [arg1, arg2, arg3, arg4, arg5]
+    return argList
 
-def PY_feedbackTargeting_compute( argList ):
-	# NOTE: argList[0] is the call Mode, which is either None or 'compute'
-	import numpy as np
-	global PY_feedbackTargeting_init
-	global PY_feedbackTargeting_Inputs
-	if PY_feedbackTargeting_init < 0 :
-		PY_feedbackTargeting_init = 1
-		PY_feedbackTargeting_Inputs = g_PluginArrayInterfaceHash['PY_feedbackTargeting_Inputs']
 
-	# Get inputs
-	att = np.array(argList[PY_feedbackTargeting_Inputs['att']])
-	erratt = np.array(argList[PY_feedbackTargeting_Inputs['erratt']])
-	IMtxInput = np.array(argList[PY_feedbackTargeting_Inputs['IMtx']])
+def PY_feedbackTargeting_compute(argList):
+    # NOTE: argList[0] is the call Mode, which is either None or 'compute'
+    import numpy as np
 
-	# Since STK gives inertia matrix as 9x1, let's convert to 3x3 matrix instead
-	IMtx = np.array([IMtxInput[0:3], IMtxInput[3:6], IMtxInput[6:9]])
+    global PY_feedbackTargeting_init
+    global PY_feedbackTargeting_Inputs
+    if PY_feedbackTargeting_init < 0:
+        PY_feedbackTargeting_init = 1
+        PY_feedbackTargeting_Inputs = g_PluginArrayInterfaceHash[
+            "PY_feedbackTargeting_Inputs"
+        ]
 
-	# Create gains
-	k = 2
-	c = 0.8
+    # Get inputs
+    att = np.array(argList[PY_feedbackTargeting_Inputs["att"]])
+    erratt = np.array(argList[PY_feedbackTargeting_Inputs["erratt"]])
+    IMtxInput = np.array(argList[PY_feedbackTargeting_Inputs["IMtx"]])
 
-	# Apply control
-	retList = -IMtx*(k*erratt[0:3]*erratt[3]+c*att[4:7])
+    # Since STK gives inertia matrix as 9x1, let's convert to 3x3 matrix instead
+    IMtx = np.array([IMtxInput[0:3], IMtxInput[3:6], IMtxInput[6:9]])
 
-	# This is the output format STK expects
-	return np.array([retList])
+    # Create gains
+    k = 2
+    c = 0.8
+
+    # Apply control
+    retList = -IMtx * (k * erratt[0:3] * erratt[3] + c * att[4:7])
+
+    # This is the output format STK expects
+    return np.array([retList])
--- StkExtensionPlugins/PluginScripts/Python/Constraint.py
+++ StkExtensionPlugins/PluginScripts/Python/Constraint.py
@@ -8,17 +8,17 @@ PY_Constraint_init = -1
 # Constraint Plugin component PY_Constraint
 # Author:     Guy Smith
 # Company:    Analytical Graphics, Inc.
-# Description: 
-#  This function is called by the AGI Constraint Script Plugin software and is not intended 
+# Description:
+#  This function is called by the AGI Constraint Script Plugin software and is not intended
 #   for general use. This function takes a structure as input that contains
 #   a 'method' string parameter that is utilized to determine what the purpose
 #   of the call was. Four methods are supported. ( 'compute', 'register', 'GetAccessList',
-#   'GetConstraintDisplayName' ) Refer to the Constraint Script Plugin documentation 
+#   'GetConstraintDisplayName' ) Refer to the Constraint Script Plugin documentation
 #   for an in-depth explaination of the functionality.
 #
 #  This file provides an example of the capabilities of the Constraint Script Plugin
 #   functionality. The simple Constraint that is implemented by this script associates itself
-#   with the Facility class and configures itself to return the the Constraint iteration 
+#   with the Facility class and configures itself to return the the Constraint iteration
 #   step Epoch time (in STK Epoch seconds) passed in on every iteration.
 
 # Notes:
@@ -35,108 +35,125 @@ PY_Constraint_init = -1
 
 # Copyright 2006, Analytical Graphics Incorporated
 
-#==========================================================================
+# ==========================================================================
 # PY_Constraint_GetConstraintDisplayName() fctn
-#==========================================================================
-def PY_Constraint_GetConstraintDisplayName () :
-	return 'PythonPluginConstraint'
+# ==========================================================================
+def PY_Constraint_GetConstraintDisplayName():
+    return "PythonPluginConstraint"
 
-#==========================================================================
+
+# ==========================================================================
 # PY_Constraint_GetAccessList() fctn
-#==========================================================================
+# ==========================================================================
 
-G_PY_Constraint_AllClasses = 'Aircraft,AreaTarget,Facility,GroundVehicle,'
-G_PY_Constraint_AllClasses +='LaunchVehicle,Missile,Planet,Radar,Satellite,Ship,Star,Target'
-
-def PY_Constraint_GetAccessList( argList ) :
-	#str(argList[0]) is the name of the call mode 'GetAccessList'
-	strBaseClass = str(argList[1])
-
-	strReturnValue = ""
-	if strBaseClass == "Facility" :
-		strReturnValue = G_PY_Constraint_AllClasses
-
-	return strReturnValue
+G_PY_Constraint_AllClasses = "Aircraft,AreaTarget,Facility,GroundVehicle,"
+G_PY_Constraint_AllClasses += (
+    "LaunchVehicle,Missile,Planet,Radar,Satellite,Ship,Star,Target"
+)
 
 
-#==========================================================================
+def PY_Constraint_GetAccessList(argList):
+    # str(argList[0]) is the name of the call mode 'GetAccessList'
+    strBaseClass = str(argList[1])
+
+    strReturnValue = ""
+    if strBaseClass == "Facility":
+        strReturnValue = G_PY_Constraint_AllClasses
+
+    return strReturnValue
+
+
+# ==========================================================================
 # PY_Constraint fctn
-#==========================================================================
-def PY_Constraint( argList ) :
-	callMode = str(argList[0])
-	if callMode == 'None' :
-		retVal = PY_Constraint_compute( argList )	# do compute
-	elif callMode == 'register' :
-		global PY_Constraint_init 
-		PY_Constraint_init = -1
-		retVal = PY_Constraint_register()
-	elif callMode == 'compute' :
-		retVal = PY_Constraint_compute( argList )	# do compute
-	elif callMode == "GetAccessList" :
-		retVal = PY_Constraint_GetAccessList( argList )
-	elif callMode == "GetConstraintDisplayName" :
-		retVal = PY_Constraint_GetConstraintDisplayName()
-	else:
-		retVal = []	# # bad call, return empty list
-	return retVal
+# ==========================================================================
+def PY_Constraint(argList):
+    callMode = str(argList[0])
+    if callMode == "None":
+        retVal = PY_Constraint_compute(argList)  # do compute
+    elif callMode == "register":
+        global PY_Constraint_init
+        PY_Constraint_init = -1
+        retVal = PY_Constraint_register()
+    elif callMode == "compute":
+        retVal = PY_Constraint_compute(argList)  # do compute
+    elif callMode == "GetAccessList":
+        retVal = PY_Constraint_GetAccessList(argList)
+    elif callMode == "GetConstraintDisplayName":
+        retVal = PY_Constraint_GetConstraintDisplayName()
+    else:
+        retVal = []  # # bad call, return empty list
+    return retVal
 
-#==========================================================================
+
+# ==========================================================================
 # PY_Constraint_register
-#==========================================================================
-def PY_Constraint_register() :
+# ==========================================================================
+def PY_Constraint_register():
 
-	args = []
+    args = []
 
-	# Outputs
+    # Outputs
 
-	args.append('ArgumentType = Output ; ArgumentName = Status ; Name = Status ') 
-	args.append('ArgumentType = Output ; ArgumentName = Result ; Name = Result ') 
+    args.append("ArgumentType = Output ; ArgumentName = Status ; Name = Status ")
+    args.append("ArgumentType = Output ; ArgumentName = Result ; Name = Result ")
 
-	# Inputs
+    # Inputs
 
-	args.append('ArgumentType = Input ; ArgumentName = Epoch ; Name = Epoch ') 
+    args.append("ArgumentType = Input ; ArgumentName = Epoch ; Name = Epoch ")
 
-	args.append('ArgumentType = Input ; ArgumentName = fromPos ; Name = fromPosition ; RefName = Fixed ') 
+    args.append(
+        "ArgumentType = Input ; ArgumentName = fromPos ; Name = fromPosition ; RefName = Fixed "
+    )
 
-	args.append('ArgumentType = Input ; ArgumentName = fromVel ; Name = fromVelocity ; RefName = Inertial ') 
+    args.append(
+        "ArgumentType = Input ; ArgumentName = fromVel ; Name = fromVelocity ; RefName = Inertial "
+    )
 
-	args.append('ArgumentType = Input ; ArgumentName = fromQuat ; Name = fromQuaternion; RefName = Fixed ')
+    args.append(
+        "ArgumentType = Input ; ArgumentName = fromQuat ; Name = fromQuaternion; RefName = Fixed "
+    )
 
-	args.append('ArgumentType = Input ; ArgumentName = fromObj ; Name = fromObjectPath ')
- 
-	args.append('ArgumentType = Input ; ArgumentName = toPos ; Name = toPosition ; RefName = Fixed ') 
+    args.append(
+        "ArgumentType = Input ; ArgumentName = fromObj ; Name = fromObjectPath "
+    )
 
-	args.append('ArgumentType = Input ; ArgumentName = toVel ; Name = toVelocity ; RefName = Inertial ') 
+    args.append(
+        "ArgumentType = Input ; ArgumentName = toPos ; Name = toPosition ; RefName = Fixed "
+    )
 
-	args.append('ArgumentType = Input ; ArgumentName = toQuat ; Name = toQuaternion; RefName = Fixed ')
+    args.append(
+        "ArgumentType = Input ; ArgumentName = toVel ; Name = toVelocity ; RefName = Inertial "
+    )
 
-	args.append('ArgumentType = Input ; ArgumentName = toObj ; Name = toObjectPath ') 
+    args.append(
+        "ArgumentType = Input ; ArgumentName = toQuat ; Name = toQuaternion; RefName = Fixed "
+    )
 
-	# Get an input from the Vector Geometry Tool
-	descrip = 'ArgumentType = Input ; ArgumentName = toEarthFromMoonInSunFixed ; '
-	descrip += 'Type = Vector ; Name = Earth ; Source = CentralBody/Moon ; RefSource = CentralBody/Sun ;'
-	descrip += 'RefName = Fixed ; Derivative = Yes'
-	args.append(descrip) 
+    args.append("ArgumentType = Input ; ArgumentName = toObj ; Name = toObjectPath ")
 
-	return args
+    # Get an input from the Vector Geometry Tool
+    descrip = "ArgumentType = Input ; ArgumentName = toEarthFromMoonInSunFixed ; "
+    descrip += "Type = Vector ; Name = Earth ; Source = CentralBody/Moon ; RefSource = CentralBody/Sun ;"
+    descrip += "RefName = Fixed ; Derivative = Yes"
+    args.append(descrip)
 
-
-def PY_Constraint_compute( argList ) :
-	# NOTE: argList[0] is the call Mode, which is either None or 'compute'
-	global debug
-	global PY_Constraint_init
-	global PY_Constraint_Inputs
-	status = 'Okay'
-	if PY_Constraint_init < 0 :
-		PY_Constraint_init = 1
-		PY_Constraint_Inputs = g_PluginArrayInterfaceHash['PY_Constraint_Inputs']
-		status = 'MESSAGE: [Info] PY_Constraint- Everything is fine;  CONTROL: OK'
-	time = float(argList[PY_Constraint_Inputs['Epoch']])
-	
-	retList = []
-	retList.append(status) # Status
-	retList.append(time)	# return epoch as the value
-	
-	return retList
+    return args
 
 
+def PY_Constraint_compute(argList):
+    # NOTE: argList[0] is the call Mode, which is either None or 'compute'
+    global debug
+    global PY_Constraint_init
+    global PY_Constraint_Inputs
+    status = "Okay"
+    if PY_Constraint_init < 0:
+        PY_Constraint_init = 1
+        PY_Constraint_Inputs = g_PluginArrayInterfaceHash["PY_Constraint_Inputs"]
+        status = "MESSAGE: [Info] PY_Constraint- Everything is fine;  CONTROL: OK"
+    time = float(argList[PY_Constraint_Inputs["Epoch"]])
+
+    retList = []
+    retList.append(status)  # Status
+    retList.append(time)  # return epoch as the value
+
+    return retList
